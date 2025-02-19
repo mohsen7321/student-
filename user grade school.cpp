ce1654e5765d809ab52cfcdc4a9332c05f9ad8d0
mohsen7321 <mohsenkhaledd@gmail.com>
2025-02-20 01:44:37 +0200

    Add files via upload

---
+++ user grade school.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int studentID=2029;
+    int age=16;
+    char studentgrade='A';
+    float studentmark=98.5;
+    cout<<"Student ID:"<<studentID<<endl;
+    cout<<"Age:"<<age<<endl;
+    cout<<"Student grade:"<<studentgrade<<endl;
+    cout<<"Student mark:"<<studentmark<<endl;
+    return 0;
+}
